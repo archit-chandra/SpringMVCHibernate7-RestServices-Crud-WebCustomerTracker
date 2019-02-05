1e82d69c1c27dc9260f749ac3d2b3a59743c9e17
archit_chandra <send2archit.chandra@gmail.com>
2019-02-05 01:01:39 +0100

    added rest controller method to get all customers

---
+++ src/main/java/com/archit/rest/CustomerRestController.java
@@ -0,0 +1,25 @@
+package com.archit.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.archit.entity.Customer;
+import com.archit.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    // add mapping for GET "/customers"
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(){
+        return customerService.getCustomers();
+    }
+}
