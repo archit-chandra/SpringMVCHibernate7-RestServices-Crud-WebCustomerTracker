a1cb33ca98ccab199a02245c8de71e4142f3f453
archit_chandra <send2archit.chandra@gmail.com>
2019-02-06 21:36:41 +0100

    added rest controller method to add a customer

--- src/main/java/com/archit/rest/CustomerRestController.java
+++ src/main/java/com/archit/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,13 @@ public class CustomerRestController {
         if (customer == null) {
             throw new CustomerNotFoundException("Customer Id not found - " + customerId);
         }
+        return customer;
+    }
+
+    // add mapping for POST "/customers" (adding a new customer)
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
         return customer;
     }
 
