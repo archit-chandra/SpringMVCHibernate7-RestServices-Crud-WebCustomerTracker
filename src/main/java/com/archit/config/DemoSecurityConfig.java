1a2e306e73833e33d08cecc31acc4b2dbf5cc479
archit_chandra <send2archit.chandra@gmail.com>
2019-02-11 21:24:05 +0100

    secured rest endpoints by roles - updated spring config class: 'DemoSecurityConfig'

--- src/main/java/com/archit/config/DemoSecurityConfig.java
+++ src/main/java/com/archit/config/DemoSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.archit.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,8 +29,31 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         // secures all REST endpoints under "/api/customers"
+        // and adds following security authorizations
+        //
+        // EMPLOYEE role can perform following
+        // 1. Get a list of all customers.  GET /api/customers
+        // 2. Get a single customer.  GET /api/customers/{customerId}
+
+        //
+        // MANAGER role can perform following
+        // 1. Add a new customer.  POST /api/customers
+        // 2. Update an existing customer.  PUT /api/customers
+        //
+
+        //
+        // ADMIN role can perform following
+        // 1. Delete a customer.  DELETE /api/customers/{customerId}
+        //
+
         http.authorizeRequests()
-                .antMatchers("/api/customers/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/customers").hasRole("EMPLOYEE")
+                .antMatchers(HttpMethod.GET, "/api/customers/**").hasRole("EMPLOYEE")
+                .antMatchers(HttpMethod.POST, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/customers/**").hasRole("ADMIN")
                 .and()
                 .httpBasic()
                 .and()
@@ -48,12 +72,11 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Why disable sessions?
         //
-        // For our application, we would like avoid the use of cookies for session tracking. This should force the REST client
+        // For our application, we would like avoid the use of cookies for sesson tracking. This should force the REST client
         // to enter user name and password for each request. However, this is not always the case depending on the REST client / browser
         // you are using. Your mileage will vary here (for example, this doesn't work in Eclipse embedded browser).
         //
         // For more details, see this link
         // http://www.baeldung.com/spring-security-session
-
     }
 }
